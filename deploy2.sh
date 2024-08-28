2b64d98a40a6977a573d255c43c2f6138f3ac509
ec2-user <kunle_adesigbin@yahoo.com>
2024-08-28 12:34:35 -0500

    deploy

---
+++ deploy2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo List of 2024 DevOps Engineers
+echo Simon Cynthia Baba Obi
+#While working on this project, the team reached and asked me to work on another project, for this I need to back up this project using the git stash command.
